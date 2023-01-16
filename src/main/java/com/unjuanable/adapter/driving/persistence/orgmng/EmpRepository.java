723e85cc30fb4caa1fab8d6ee8dd0582c86c620e
huaying <huaying.chen@ymm56.com>
2023-01-16 11:28:34 +0800

    Initial commit

---
+++ src/main/java/com/unjuanable/adapter/driving/persistence/orgmng/EmpRepository.java
@@ -0,0 +1,11 @@
+package com.unjuanable.adapter.driving.persistence.orgmng;
+
+/**
+ * Description
+ *
+ * @author huaying
+ * @version 0.0.1
+ * @since 2023/1/16
+ */
+public class EmpRepository {
+}
---
+++ src/main/java/com/unjuanable/application/tenantmng/TenantService.java
@@ -0,0 +1,11 @@
+package com.unjuanable.application.tenantmng;
+
+/**
+ * Description
+ *
+ * @author huaying
+ * @version 0.0.1
+ * @since 2023/1/16
+ */
+public class TenantService {
+}
